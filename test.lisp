6ac9e7fb897f4764f3107c15a71b8a59e1ad6538
blasut <blasut937@gmail.com>
2017-03-28 19:14:31 +0200

    add actual tests, with some notes and example runs

--- test.lisp
+++ test.lisp
@@ -1,97 +1,152 @@
 (in-package #:cl-microkanren)
 
-;;;; tests from the repo
-(defmacro test-check (title tested-expression expected-result)
-  `(progn
-     (format t "Testing ~s\n" ,title)
-     (let* ((expected ,expected-result)
-            (produced ,tested-expression))
-       (or (equalp expected produced)
-           (format nil "Failed: ~a~%Expected: ~a~%Computed: ~a~%" 'tested-expression expected produced)))))
+(setq prove:*default-test-function* #'equalp)
 
-(defparameter a-and-b
-  (conj
-    (call/fresh (lambda (a) (== a 7)))
-    (call/fresh (lambda (b) (disj (== b 5) (== b 6))))))
+(subtest "walk"
+  (subtest "nested references"
+    ;; walk should get the actual value of the lvar if it exists
+    (is (walk (lvar 0)
+              (ext-s (lvar 0) (lvar 1) (ext-s (lvar 1) 7 nil)))
+        7)
 
-(defun fives (x)
-  (disj
-   (== x 5)      
-   (lambda (a/c)
-      (lambda ()
-        (funcall (fives x) a/c)))))
+    (is (walk (lvar 0)
+              (ext-s (lvar 0) (lvar 1)
+                     (ext-s (lvar 1) (lvar 2)
+                            (ext-s (lvar 2) (lvar 3) nil))))
+
+        (lvar 3))
+
+    (is (walk (lvar 0)
+              (ext-s (lvar 0) (lvar 1)
+                     (ext-s (lvar 1) 5
+                            (ext-s (lvar 2) (lvar 3) nil))))
+        5))
+
+  (subtest "should treat nil as values"
+    (is (walk '() (ext-s (lvar 0) '() nil))
+        '())
+
+    (is (walk (lvar 1) (ext-s (lvar 0) (lvar 1)
+                              (ext-s (lvar 1) '() nil)))
+        '())
+
+    (is (walk (lvar 1) nil)
+        '())
+
+    (is (walk (lvar 0) (ext-s (lvar 0) '() nil))
+        '()))
+
+  (subtest "things that are not lvars should be returned"
+    (is (walk 5 (ext-s (lvar 0) 5 nil))
+        5)
+
+    (is (walk '(123) (ext-s (lvar 0) 5 nil))
+        '(123))
+
+    (is (walk 5 nil)
+        5)))
 
 
-(test-check "second-set t1"
-  (let (($ (call/empty-state (call/fresh (lambda (q) (== q 5))))))
-    (car $))
-  '(((#(0) . 5)) . 1))
+(subtest "unify"
+  (subtest "unify should return false instead of nil when false"
+    (is (unify 1 2 empty-state)
+        'false)
 
-(test-check "second-set t2"
-  (let (($ (call/empty-state (call/fresh (lambda (q) (== q 5))))))
-    (cdr $))
-  '())
+    (is (unify 1 2 nil)
+        'false)
 
-(test-check "second-set t3"
-  (let (($ (call/empty-state a-and-b)))
-    (car $))
-  '(((#(1) . 5) (#(0) . 7)) . 2))
+    (is (unify 1 1 empty-state)
+        empty-state)
 
-(test-check "second-set t4"
-  (let (($ (call/empty-state a-and-b)))
-    (car (cdr $)))
-  '(((#(1) . 6) (#(0) . 7)) . 2))
+    (is (unify #(0) 6 '((#(0) . 5)))
+        'false)
 
-(test-check "second-set t5"
-  (let (($ (call/empty-state a-and-b)))
-    (cdr (cdr $)))
-  '())
+    (is (unify 1 1 '())
+        '())))
 
-(test-check "who cares"
-  (let (($ (call/empty-state (call/fresh (lambda (q) (fives q))))))
-    (take 1 $))
-  '((((#(0) . 5)) . 1)))
+(subtest "=="
+  (subtest "should return empty list for no match"
+    (is (call/empty-state (call/fresh (lambda (b) (conj (== b 5) (== b 6)))))
+        '())
 
-(test-check "super simple"
-  (let (($ (call/empty-state (call/fresh (lambda(q) (== q 5))))))
-    (take 1 $))
-  '((((#(0) . 5)) . 1)))
+    (is (call/empty-state (call/fresh (lambda (b) (conj (== b '())
+                                                        (== b '())))))
+        '((((#(0))) . 1)))
 
-(test-check "appendo"
-  (run* (q x y) (== `(,x ,y) q) (appendo x y '(1 2 3 4 5)))
-  '((NIL (1 2 3 4 5)) ((1) (2 3 4 5)) ((1 2) (3 4 5)) ((1 2 3) (4 5))
-           ((1 2 3 4) (5)) ((1 2 3 4 5) NIL)))
+    (UNIFY #(0) '(#(1) #(2)) '((#(2) 1 2) (#(1))))
 
-(test-check "acorn"
-  (run* (q) (== '(a c o r n) q))
-  '((A C O R N)))
+    (UNIFY '(#(3) . #(5)) '(1 2) '((#(1) #(3) . #(4))))
 
-(defun fives (x) (disj (== 5 x) (zzz (fives x))))
+    ;; this run:
+    (run 1 (q) (fresh (x)
+                      (== `(a c ,x r n) q)
+                      (== x '(1 2 3))))
+    (UNIFY (A C #(1) R N) #(0) NIL)
+    (WALK (A C #(1) R N) NIL)
+    (EXT-S #(0) (A C #(1) R N) NIL)
 
-(defun test-recursion ()
-  (let ((res (call/empty-state (call/fresh (lambda(q) (fives q))))))
-      (assert (equalp '(((#(0) . 5)) . 1) (car res)))
-      (assert (functionp (cdr res)))))
+    (UNIFY #(1) (1 2 3) ((#(0) A C #(1) R N)))
+    (WALK #(1) ((#(0) A C #(1) R N)))
+    (WALK (1 2 3) ((#(0) A C #(1) R N)))
 
-(test-recursion)
+    (EXT-S #(1) (1 2 3) ((#(0) A C #(1) R N)))
 
-(defun fives+ (x) (disj+ (== 5 x) (fives+ x)))
-(defun sixes+ (x) (disj+ (== 6 x) (sixes+ x)))
-(defun fives-and-sixes+ (x)
-  (disj+ (conj+ (fives+ x) (== x 5)))
-  (sixes+ x))
+    
+    ;; this run:
+    (run 1 (q) (fresh (x y)
+                      (== `(a c ,x r n) q)
+                      (== `(m j a ,y) x)
+                      (== y '(1 2 3))))
 
-;; treat '() as truthy
-(assert (equal (scm-and '() 'a 'b 'c) 'c))
-(assert (equal (scm-and '() 'a 'b 'false 'c) 'b))
+    (== (A C #(1) R N) #(0))
+    (UNIFY (A C #(1) R N) #(0) NIL)
 
-(assert (equal (scm-and 'false 'false 'false) 'false))
+    (== (M J A #(2)) #(1))
+    (UNIFY (M J A #(2)) #(1) ((#(0) A C #(1) R N)))
 
-(assert (equal (scm-or '() 'a 'b 'false 'c) '()))
-(assert (equal (scm-or 'false 'false '() 'false 'c) '()))
-(assert (equal (scm-or 'false 'false) nil))
-(assert (equal (scm-or 'a 'b '()) 'a))
 
-(assert (equal (scm-if 'false 'a 'b) 'b))
-(assert (equal (scm-if () 'a 'b) 'a))
+    ;; this run:
+    (is (unify '(A B #(1)) '(A B #(0)) NIL)
+        '((#(1) . #(0))))
+
+    ;; the value retuned is _.0
+    ;; it is because #(1) is pointing to #(0)
+    ;; #(0) is pointning to nothing
+    ;; which means it is unbound
+    (run 1 (q) (fresh (x)
+                      (== `(a b ,x) `(a b ,q))))
+
+    (== (A B #(1)) (A B #(0)))
+    (UNIFY (A B #(1)) (A B #(0)) NIL)
+    (WALK (A B #(1)) NIL)
+    (WALK (A B #(0)) NIL)
+
+    ;; both U & V are pairs in this scenario
+    (UNIFY A A NIL)               ;; returns NIL == empty-state
+    (UNIFY (B #(1)) (B #(0)) NIL) ;; returns nil
+    ;; because u and v ar pairs, we walk again with the car
+    (UNIFY B B NIL)
+    ;; ... and the cdr
+    (UNIFY (#(1)) (#(0)) NIL)
+    ;; which are pairs...
+    (UNIFY #(1) #(0) NIL)
+    ;; they are both lvars but not equal
+    (LVAR=? #(1) #(0))
+    ;; so extend the state, pointing #(1) to #(0)
+    (EXT-S #(1) #(0) NIL)
+    ;; then check the CDR
+    (UNIFY NIL NIL ((#(1) . #(0))))
+    ;; this returns the state unchanged
+    ;; because (eqv? nil nil) == T
+    
+
+    ;; this run
+    (run 1 (q) (fresh (x y)
+                      (== q x)
+                      (== `(a b ,x) `(a b c)) ))
+
+    
+    (print "end")))
+
+(finalize)
 
